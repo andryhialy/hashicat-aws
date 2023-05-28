2b6789e4095322ac309fcee5bdcd2d30d1093dca
andryhialy <ahr.dsc@gmail.com>
2023-05-28 00:45:55 +0000

    Added s3 bucket module, prise 2

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,7 +1,7 @@
 module "s3_bucket" {
   source = "terraform-aws-modules/s3-bucket/aws"
 
-  bucket = "my-s3-bucket"
+  bucket = "my-s3-bucket-2023-05-27"
   acl    = "private"
   prefix = var.prefix
 
